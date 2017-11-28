e88dd156a7b7df799318f5dc6a8a3c31446de8ff
engstrom <dustin.engstrom@vacasa.com>
2017-11-28 10:59:04 -0700

    Reorganize package structure.
    TNR-15

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='VacasaConnect',
+    version='0.1.0',
+    description='A Python 3.6 SDK for the connect.vacasa.com API.',
+    packages=['vacasa'],
+    url='https://github.com/Vacasa/python-vacasa-connect-sdk',
+    license='MIT',
+    long_description=open('README.md').read()
+)
