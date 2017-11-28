5669cd780e94364432780a507672a0fa1f2d604d
engstrom <dustin.engstrom@vacasa.com>
2017-11-28 14:48:54 -0700

    Reorganize to prevent future package namespace issues.
    TNR-15

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='VacasaConnect',
     version='0.1.0',
     description='A Python 3.6 SDK for the connect.vacasa.com API.',
-    packages=['vacasa'],
+    packages=['vacasa.connect'],
     url='https://github.com/Vacasa/python-vacasa-connect-sdk',
     license='MIT',
     long_description=open('README.md').read()
