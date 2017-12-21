d7141ea73b5e8f19e55f2db1a53b6a17eb898254
engstrom <dustin.engstrom@vacasa.com>
2017-12-21 13:19:46 -0700

    TNR-69: Add meta_param tests.

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -1,5 +1,6 @@
-import mock
 import pendulum
+from unittest.mock import patch, MagicMock
+
 from vacasa.connect import VacasaConnect
 
 
@@ -14,12 +15,12 @@ class TestVacasaConnect(object):
         'expires_at': pendulum.now().add(days=1).to_datetime_string()
     }
 
-    @mock.patch.object(VacasaConnect, '_populate_tokens')
+    @patch.object(VacasaConnect, '_populate_tokens')
     def mock_connect(self, mock_populate_tokens):
         connect = VacasaConnect('fake_key', 'fake_secret', 'fake_url')
 
         # mock call that generates tokens
-        mock_response = mock.Mock()
+        mock_response = MagicMock()
         mock_response.return_value = None
         mock_populate_tokens.return_value = mock_response
 
@@ -36,3 +37,19 @@ class TestVacasaConnect(object):
         expected = '87446b676b79b6e493a6b852ec3c32faf579086bd12728178197ef278ec7abfc'
 
         assert signature == expected
+
+    def test_add_meta_param_adds_one_to_empty(self):
+        connect = self.mock_connect()
+        params = {}
+        params = connect._add_meta_param(params, 'include_photos')
+        expected = {'include_meta': ',include_photos'}
+
+        assert params == expected
+
+    def test_add_meta_param_adds_another_to_existing(self):
+        connect = self.mock_connect()
+        params = {'include_meta': ',include_photos'}
+        params = connect._add_meta_param(params, 'amenities_map')
+        expected = {'include_meta': ',include_photos,amenities_map'}
+
+        assert params == expected
