e470c1bdeb7479d7ece90a4c56ad7b8edda9f57e
Cody Helbling <helblingcody@gmail.com>
2019-01-14 13:00:30 -0700

    TNR-2038-cancel-reservation (#30)
    * Added reservations-abandoned endpoint.
    
    * Added a docstring.
    
    * Updated a docstring.
    
    * Version bump.
    
    * Updated docstrings to match standard.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='VacasaConnect',
-    version='0.7.0',
+    version='0.7.1',
     description='A Python 3.6 SDK for the connect.vacasa.com API.',
     packages=['vacasa.connect'],
     url='https://github.com/Vacasa/python-vacasa-connect-sdk',
