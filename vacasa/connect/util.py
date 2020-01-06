b014e1a47a4a970056515585f9d4277d1e6564dc
soconnor71 <simon.oconnor@vacasa.com>
2020-01-06 13:46:08 -0700

    TNR-3365: quietened down tests

--- vacasa/connect/util.py
+++ vacasa/connect/util.py
@@ -10,15 +10,24 @@ logger = logging.getLogger(__name__)
 
 def log_http_error(r: Response):
     """ Log any 4XX/5XX error responses and re-raise """
-    try:
-        r.raise_for_status()
-    except RequestException as e:
+    if r.status_code < 500:
         try:
-            logger.exception(r.json())
-        except ValueError:
-            logger.exception(r.content)
-        raise e
-
+            r.raise_for_status()
+        except RequestException as e:
+            try:
+                logger.info(r.json())
+            except ValueError:
+                logger.info(r.content)
+            raise e
+    else:
+        try:
+            r.raise_for_status()
+        except RequestException as e:
+            try:
+                logger.exception(r.json())
+            except ValueError:
+                logger.exception(r.content)
+            raise e
 
 def is_https_url(url: str) -> bool:
     return urlparse(url).scheme.lower() == 'https'
