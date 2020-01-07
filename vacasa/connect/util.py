a16210719e95242244fab18e6305be291b605fcb
soconnor71 <simon.oconnor@vacasa.com>
2020-01-07 09:48:38 -0700

    TNR-3365: improve code layout

--- vacasa/connect/util.py
+++ vacasa/connect/util.py
@@ -10,24 +10,16 @@ logger = logging.getLogger(__name__)
 
 def log_http_error(r: Response):
     """ Log any 4XX/5XX error responses and re-raise """
-    if r.status_code < 500:
+    try:
+        r.raise_for_status()
+    except RequestException as e:
+        log_level = logging.INFO if r.status_code < 500 else logging.ERROR
         try:
-            r.raise_for_status()
-        except RequestException as e:
-            try:
-                logger.info(r.json())
-            except ValueError:
-                logger.info(r.content)
-            raise e
-    else:
-        try:
-            r.raise_for_status()
-        except RequestException as e:
-            try:
-                logger.exception(r.json())
-            except ValueError:
-                logger.exception(r.content)
-            raise e
+            logger.log(log_level, r.json())
+        except ValueError:
+            logger.log(log_level, r.content)
+        raise e
+
 
 def is_https_url(url: str) -> bool:
     return urlparse(url).scheme.lower() == 'https'
