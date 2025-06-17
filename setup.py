e1b9cbf35d03a82a09557e5becf90bff3582ad87
Martin Škára <jsem@skaramart.in>
2025-06-17 10:35:03 +0200

    FLD-616 update library to pendulum 3 (#145)

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ from setuptools import setup
 
 setup(
     name='VacasaConnect',
-    version='4.29.0',
-    description='A Python 3.6+ SDK for the connect.vacasa.com API.',
+    version='5.0.0',
+    description='A Python 3.8+ SDK for the connect.vacasa.com API.',
     packages=['vacasa.connect'],
     url='https://github.com/Vacasa/python-vacasa-connect-sdk',
     license='MIT',
     install_requires=[
-        'pendulum==2.*',
+        'pendulum==3.*',
         'requests>=2.20.0',
         'python-jose==3.*'
     ],
     extras_require={
         "cryptography": ['cryptography==38.0.*']
     },
+    python_requires='>=3.8',
     long_description=open('README.md').read()
 )
